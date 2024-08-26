415dc983bd4cdaa007a0bd3ed1c09f060e9e133e
= <=>
2024-08-26 10:20:47 -0300

    Museo

--- Museo/MuseoProject/MUseoApp/urls.py
+++ Museo/MuseoProject/MUseoApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import MUseoApp_views
+from . import views as MUseoApp_views
 
 urlpatterns = [
     path('', views.home, name='home'),
